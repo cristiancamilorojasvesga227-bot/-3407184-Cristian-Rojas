33fc51059251c7ac1d9f1643b29130b15c7139af
CristianRojas <cristian.camilorojasvesga227@gmail.com>
2026-03-17 13:36:01 -0500

    Add files via upload

---
+++ 3407184-Cristian-Rojas/week-03/starter/script.js
@@ -0,0 +1,7 @@
+// Archivo para tu código JavaScript
+// Pega aquí tu código o comienza a escribir.
+
+// Ejemplo simple:
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log('Tu código JS está listo');
+// });
